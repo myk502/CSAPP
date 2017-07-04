2ad623782dd43b37635c2f7ca13a13ac72f80798
fanese <525039107@qq.com>
2017-07-04 14:32:09 +0800

    The first commit,including the English src and pdf

---
+++ Chapter2/show_byte.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+typedef unsigned char *byte_pointer;
+
+void show_byte(byte_pointer start,int offset)
+{
+    for(int i=0;i<offset;i++)
+        printf("%.2x",start[i]);
+    printf("\n");
+}
+
+void show_int(int x)
+{
+    show_byte( (byte_pointer) &x,sizeof(int) );
+}
+
+void show_float(float x)
+{
+    show_byte( (byte_pointer) &x,sizeof(float) );
+}
+
+void show_pointer(void *x)
+{
+    show_byte( (byte_pointer) &x,sizeof(void*) );
+}
